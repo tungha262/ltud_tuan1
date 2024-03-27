5d0a99ebebcba2078927ed912cd3598ceb5d9136
tungha <tungha2206@gmail.com>
2024-03-27 11:40:06 +0700

    ..

--- app/src/main/java/com/example/myapplication/tuan1/Tuan1MainActivity.java
+++ app/src/main/java/com/example/myapplication/tuan1/Tuan1MainActivity.java
@@ -14,21 +14,20 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.myapplication.R;
 
 public class Tuan1MainActivity extends AppCompatActivity {
-    EditText txt1, txt2;
+    EditText t1, t2;
 
-    Button bt1;
+    Button btn1;
     TextView tv1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_tuan1_main);
-        txt1 = findViewById(R.id.demo1);
-        txt2 = findViewById(R.id.demo2);
-        bt1 = findViewById(R.id.demo3);
+        t1 = findViewById(R.id.demo1);
+        t2 = findViewById(R.id.demo2);
+        btn1 = findViewById(R.id.demo3);
         tv1 = findViewById(R.id.demo4);
-        bt1.setOnClickListener(v->{
-            //goi ham tinh tong
+        btn1.setOnClickListener(v->{
             tinhTong();
         });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -37,12 +36,11 @@ public class Tuan1MainActivity extends AppCompatActivity {
             return insets;
         });
     }
-    // dinh nghia ham tinh tong
+
     private void tinhTong() {
-        // lay du lieu nhap vao tu Edittex1
-        String str1 = txt1.getText().toString();
-        float so1 = Float.parseFloat(str1); // chuyen du lieu sang so
-        String str2 = txt2.getText().toString();
+        String str1 = t1.getText().toString();
+        float so1 = Float.parseFloat(str1);
+        String str2 = t2.getText().toString();
         float so2 = Float.parseFloat(str2);
         float tong = so1+so2;
 
